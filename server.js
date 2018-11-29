cd154c47517fcd83113c72d7770e64a491bafe10
DaniloGN <dan_guarizzo@hotmail.com>
2018-11-29 00:44:39 -0200

    cors

--- server.js
+++ server.js
@@ -14,20 +14,14 @@ mongoose.connect(uri);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var routes = require('./api/routes/iSteamRoutes'); //importing route
 routes(app); //register the route
-app.use(function (req, res, next) {
-	    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Pass to next layer of middleware
-    next();
-});
-
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
   });
